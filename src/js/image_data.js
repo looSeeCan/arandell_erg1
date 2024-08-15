fb8d48bd22e477d9b3045a0b23a07cd1a0985e40
looSeeCan <lly@icstars.org>
2024-08-14 22:01:53 -0500

    ....

---
+++ src/js/image_data.js
@@ -0,0 +1,75 @@
+const imagesData = [
+  {
+    link: "./html/anotherPage.html",
+    imageSrc: "../assets/support_and_networking.jpg",
+    hoverImageSrc: "../assets/support_and_networking.jpg",
+    altImageSrc: "../assets/support_and_networking.jpg",
+    alt: "Support And Networking",
+    arrowSrc:
+      "https://csimg.nyc3.digitaloceanspaces.com/Gallery/orange-arrow-right.svg",
+    subtitle: "Support And Networking",
+    description:
+      "Support network for employees with similar backgrounds or interests, facilitating networking opportunities and creating a sense of belonging.",
+  },
+  {
+    link: "./html/anotherPage.html",
+    imageSrc: "../assets/careers_qr-code.png",
+    hoverImageSrc: "../assets/careers_qr-code.png",
+    altImageSrc: "../assets/careers_qr-code.png",
+    alt: "QR Code",
+    arrowSrc:
+      "https://csimg.nyc3.digitaloceanspaces.com/Gallery/orange-arrow-right.svg",
+    subtitle: "Professional Development",
+    description:
+      "Mentoring programs, workshops, and events that help members develop new skills, enhance their careers, and gain visibility within the organization.",
+  },
+  {
+    link: "./html/messageBoard.html",
+    imageSrc: "../assets/our_mission1.jpeg",
+    hoverImageSrc: "../assets/our_mission1.jpeg",
+    altImageSrc: "../assets/our_mission1.jpeg",
+    alt: "Message Board",
+    arrowSrc:
+      "https://csimg.nyc3.digitaloceanspaces.com/Gallery/orange-arrow-right.svg",
+    subtitle: "Message Board",
+    description: "Its the Message Board👍!",
+  },
+  // {
+  //   link: "./html/anotherPage.html",
+  //   imageSrc: careersQr,
+  //   hoverImageSrc: careersQr,
+  //   altImageSrc: careersQr,
+  //   alt: "Community Engagement",
+  //   arrowSrc:
+  //     "https://csimg.nyc3.digitaloceanspaces.com/Gallery/orange-arrow-right.svg",
+  //   subtitle: "Community Engagement",
+  //   description:
+  //     "Engage in community outreach and social responsibility activities, helping to build a positive image of the organization and providing employees with opportunities to give back.",
+  // },
+  // {
+  //   link: "./html/anotherPage.html",
+  //   imageSrc: diversity,
+  //   hoverImageSrc: diversity,
+  //   altImageSrc: diversity,
+  //   alt: "Diversity",
+  //   arrowSrc:
+  //     "https://csimg.nyc3.digitaloceanspaces.com/Gallery/orange-arrow-right.svg",
+  //   subtitle: "Diversity And Inclusion",
+  //   description:
+  //     "Promoting diversity and inclusion within the workplace. They often collaborate with HR and leadership to influence policies and practices, ensuring they are inclusive and equitable.",
+  // },
+  // {
+  //   link: "./html/anotherPage.html",
+  //   imageSrc: careersQr,
+  //   hoverImageSrc: careersQr,
+  //   altImageSrc: careersQr,
+  //   alt: "Advocacy and Awareness",
+  //   arrowSrc:
+  //     "https://csimg.nyc3.digitaloceanspaces.com/Gallery/orange-arrow-right.svg",
+  //   subtitle: "Advocacy and Awareness",
+  //   description:
+  //     "Raise awareness about issues affecting their members and advocate for changes that benefit both the employees and the organization.",
+  // },
+];
+
+export default imagesData;
---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+import imagesData from "../data/images_data.js";
+
+import Image from "./image_gallery.js";
+
+// Create instances of the Image class
+const imageInstances = imagesData.map(
+  (data) =>
+    new Image(
+      data.link,
+      data.imageSrc,
+      data.hoverImageSrc,
+      data.altImageSrc,
+      data.alt,
+      data.arrowSrc,
+      data.subtitle,
+      data.description
+    )
+);
+
+// Render each image
+imageInstances.forEach((image) => image.render());
